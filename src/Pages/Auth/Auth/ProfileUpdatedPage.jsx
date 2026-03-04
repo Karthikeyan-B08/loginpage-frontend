67e5b70f55b6f8f283e75287e5570ec044e05048
Karthikeyan-B08 <bkarthikeyan659@gmail.com>
2026-03-04 21:56:28 +0530

    Updated API URL and CORS config

--- src/Pages/Auth/Auth/ProfileUpdatedPage.jsx
+++ src/Pages/Auth/Auth/ProfileUpdatedPage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../../context/AuthContext";
 import "./Profile.css";
+import axios from "axios";
 
 function ProfileUpdatePage() {
 
